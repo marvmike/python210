23c86338ab7519e37f3a70d3464e3f05516b29b4
Marvellous-Michael Olanrewaju <micola201100@gmail.com>
2025-01-08 13:48:18 +0000

    first commit

---
+++ starts.py
@@ -0,0 +1,13 @@
+age = 25
+year = 2023
+
+print("Age:", age)
+print("Year:", year)
+
+user_input = input("Enter a string: ")
+
+if user_input == user_input[::-1]:
+    print("The string is a palindrome.")
+else:
+    print("The string is not a palindrome.")
+
